ba65e95780240b660e9aa9128ee2028e770a4c7e
itsvinayak <itssvinayak@gmail.com>
2019-09-04 11:59:36 +0530

    packaged

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
+
+    def __repr__(self):
+        return self.text
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy 
+from flask import render_template, request, redirect, url_for
+from app import app
+from app.models import Todo
+from app import db
 
-app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/somil/Desktop/todo/todo.db'
-
-db = SQLAlchemy(app)
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(200))
-    complete = db.Column(db.Boolean)
- 
 @app.route('/')
 def index():
     incomplete = Todo.query.filter_by(complete=False).all()
     complete = Todo.query.filter_by(complete=True).all()
 
     return render_template('index.html', incomplete=incomplete, complete=complete)
+
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -27,14 +20,12 @@ def add():
 
     return redirect(url_for('index'))
 
+
 @app.route('/complete/<id>')
 def complete(id):
 
     todo = Todo.query.filter_by(id=int(id)).first()
     todo.complete = True
     db.session.commit()
-    
-    return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    return redirect(url_for('index'))
